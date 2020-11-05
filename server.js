935d5c2778c1c3c745d9216a234e56c2b4d53c11
azjustin1 <48974108+azjustin1@users.noreply.github.com>
2020-11-05 11:09:42 +0700

    initiate

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import http from "http";
+import socket from "socket.io";
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is up");
+});
+
+io.on("connection", (socket) => {
+  console.log(`Connected: ${socket.id}`);
+
+  socket.on("disconnect", () => console.log(`Disconnected: ${socket.id}`));
+
+  socket.on("join", (room) => {
+    console.log(`Socket ${socket.id} joining ${room}`);
+    socket.join(room);
+  });
+
+  socket.on("chat", ({ room, name, message }) => {
+    // Send to all the client in the room
+    socket.broadcast.emit("new-message", { room, name, message });
+    // Sender to the sender
+    socket.emit("new-message", { room, name, message });
+  });
+});
+
+server.listen(9000, () => {
+  console.log(`Server is running at http://localhost:9000`);
+});
